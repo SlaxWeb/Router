f42f9bf224f090652167f43acc45af509efb045c
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-04-24 10:50:05 +0200

    Properly parse CLI parameters

--- Request.php
+++ Request.php
@@ -24,14 +24,22 @@ class Request
         return null;
     }
 
+    public function __toString()
+    {
+        return "{$this->_domain} ({$this->_method}) {$this->_uri}";
+    }
+
     protected function _setUp()
     {
         // check if request is from CLI
         if (php_sapi_name() === "cli") {
-            $options = getopt();
-            array_shift($options);
-            $this->_uri = implode("/", $options);
+            $options = getopt("u:", ["uri:"]);
+            if (isset($options["u"])) {
+                $options["uri"] = $options["u"];
+            }
+            $this->_uri = $options["uri"];
             $this->_method = "CLI";
+            $this->_domain = "Command Line";
         } else {
             // normal WEB request
             if (isset($_SERVER["REQUEST_METHOD"]) === false) {
