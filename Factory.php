629812b7d66991bcdac915498ddbcf7d5234decf
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-01-15 14:06:54 +0100

    Extend router factory
    break functionality into multiple methods, and prepare factory to inject
    Response component to router

--- Factory.php
+++ Factory.php
@@ -1,13 +1,35 @@
 <?php
+/**
+ * Request router Factory
+ *
+ * SlaxWeb\Router Factory instantiates the Request and Response components
+ * and injects them into the Router.
+ *
+ * @author Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @package \SlaxWeb\Router
+ * @version v0.3
+ * @license MIT
+ *
+ * @copyright (c) 2015 Tomaz Lovrec
+ */
 namespace SlaxWeb\Router;
 
-use SlaxWeb\Router\Router;
-use SlaxWeb\Router\Request;
+use SlaxWeb\Router\Router as Router;
+use SlaxWeb\Router\Request as Request;
+use SlaxWeb\Router\Response as Response;
 
-class Factory
+class RouterFactory
 {
-    public static function init()
+    public static function init(\Psr\Log\LoggerInterface $logger = null)
     {
+        return static::_initRouter(
+            static::_initRequest(),
+            static::_initResponse($logger)
+        );
+    }
+
+	protected static function _initRequest()
+	{
         $request = new Request;
         if (php_sapi_name() === "cli") {
             $options = getopt("u:", ["uri:"]);
@@ -28,9 +50,17 @@ class Factory
             );
         }
 
+        return $request;
+	}
+
+	protected static function _initResponse(\Psr\Log\LoggerInterface $logger = null)
+	{
+        return new Response($logger);
+	}
+
+	protected static function _initRouter(Request $request, Response $response)
+	{
         $router = new Router($request);
         Helper::init($router, $request);
-
-        return $router;
-    }
+	}
 }
