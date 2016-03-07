c185878d6a981db1f3ec2c9ff4e4523b6a1fcd5a
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-03-07 13:34:41 +0100

    add empty dispatcher test

---
+++ tests/unit/DispatcherTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Dispatcher Tests
+ *
+ * Dispatcher is the main class of the Router component, it must find the
+ * corresponding Route to the retrieved Request, and execute that Routes
+ * callable definition and return the Response object.
+ *
+ * @package   SlaxWeb\Router
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.3
+ */
+namespace SlaxWeb\Router\Tests\Unit;
+
+class DispatcherTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+}
